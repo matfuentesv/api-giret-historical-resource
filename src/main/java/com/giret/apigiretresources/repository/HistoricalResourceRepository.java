7ecce8cac2f33246c1f0a34ab5aef892ea223049
O009920 <mfuentes@forum.cl>
2025-06-27 19:32:24 -0400

    subida codigo fuente api

---
+++ src/main/java/com/giret/apigiretresources/repository/HistoricalResourceRepository.java
@@ -0,0 +1,12 @@
+package com.giret.apigiretresources.repository;
+
+
+import com.giret.apigiretresources.model.HistoricalResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoricalResourceRepository extends JpaRepository<HistoricalResource, Long> {
+
+
+
+
+}
